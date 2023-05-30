260e60cee039d3212969c336eabfe35c7527be51
Juan David Garcia <>
2023-05-29 19:32:03 -0500

    add initial TypeOrm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,22 @@
+import { join } from 'path';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'ecommerce',
+      password: 'password',
+      database: 'ecommerce',
+      entities: [join(__dirname, '**', '*.entity.{ts,js}')],
+      autoLoadEntities: true,
+      synchronize: true,
+      logging: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
