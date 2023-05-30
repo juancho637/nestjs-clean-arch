aae4d4e5e4ae489bcabb660092aad637bf72b4c8
Juan David Garcia <>
2023-05-29 19:30:39 -0500

    add config project

---
+++ src/config/app.config.ts
@@ -0,0 +1,4 @@
+export const appConfig = {
+  name: '',
+  env: '',
+};
---
+++ src/config/database.config.ts
@@ -0,0 +1,14 @@
+export const databaseConfig = {
+  driver: 'pgsql',
+  url: '',
+  host: '',
+  port: '',
+  database: '',
+  username: '',
+  password: '',
+  charset: 'utf8',
+  prefix: '',
+  prefix_indexes: true,
+  search_path: 'public',
+  sslmode: 'prefer',
+};
