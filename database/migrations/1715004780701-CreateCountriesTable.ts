324990539c3369385c87110bfc8408047cb4b0e5
Juan David Garcia <juancho637@gmail.com>
2025-04-21 21:20:54 -0500

    chage country migration name

--- database/migrations/1715004780701-CreateCountriesTable.ts
+++ database/migrations/1715004780701-CreateCountriesTable.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export class CreateCountriesTable1745180794015 implements MigrationInterface {
+export class CreateCountriesTable1715004780701 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
