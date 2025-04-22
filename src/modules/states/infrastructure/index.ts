2effeeff4097ae77f6dc19369881411560524741
Juan David Garcia <juancho637@gmail.com>
2025-04-22 12:49:01 -0500

    Update index.ts

--- src/modules/states/infrastructure/index.ts
+++ src/modules/states/infrastructure/index.ts
@@ -1,5 +1,5 @@
 export * from './api';
-export * from './states.module';
 export * from './persistence';
 export * from './seeders';
 export * from './state.presenter';
+export * from './states.module';
